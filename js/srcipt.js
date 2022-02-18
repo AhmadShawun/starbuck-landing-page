968f1d506ffc0f775b0dc99bdd03bf7d73b605e0
Ahmed Shawun <faysalahmadshawun@gmail.com>
2022-02-18 23:30:23 +0600

    done-project

---
+++ js/srcipt.js
@@ -0,0 +1,7 @@
+function imgSlider(anything) {
+    document.querySelector(".starbucks").src = anything;
+}
+function changebgcolor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
